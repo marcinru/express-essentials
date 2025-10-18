26c6f3e5ab4f721474d64f83f785b7ad92faf438
Marcin Er <mar.rudzki@gmail.com>
2025-10-18 16:43:48 +0200

    Add mock data

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import fs from 'fs';
+
+const data = JSON.parse(
+  fs.readFileSync(new URL('./data/mock.json', import.meta.url), 'utf8')
+);
 
 const app = express();
 
@@ -6,4 +11,5 @@ const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`);
+  console.log(data);
 });

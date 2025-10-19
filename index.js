a41de6ee786385e4cee584355b5068ac616b3a0d
Marcin Er <mar.rudzki@gmail.com>
2025-10-19 06:50:28 +0200

    Return JSON data from a GET endpoint

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('This is a GET request at / endpoint');
+  res.json(data);
 });
 
 app.post('/create', (req, res) => {

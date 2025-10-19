fbaa4d964214369a3d789e172d6d2fee47fc0a79
Marcin Er <mar.rudzki@gmail.com>
2025-10-19 06:30:34 +0200

    Add CRUD routes

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.send('This is a GET request at / endpoint');
+});
+
+app.post('/create', (req, res) => {
+  res.send('This is a POST request at /create endpoint');
+});
+
+app.put('/edit', (req, res) => {
+  res.send('This is a PUT request at /edit endpoint');
+});
+
+app.delete('/delete', (req, res) => {
+  res.send('This is a DELETE request at /delete endpoint');
+});
+
 app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`);
   console.log(data);
